dfd3f5266b369efe30e49283374a3c80ac1f9d4f
awsome-cats <cattwin82@gmail.com>
2021-07-21 00:22:02 +0900

    change layout

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('<h2>Express app app</h2><p>version 1.0</p>')
+    res.send('<h2>Express app app</h2><p>version 2.0</p>')
 })
 
 app.get('/products', (req, res) => {
@@ -17,6 +17,10 @@ app.get('/products', (req, res) => {
         {
             productID:2,
             price: 100
+        },
+        {
+            productID:3,
+            price: 200
         }
     ])
 })
